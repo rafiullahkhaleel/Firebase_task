e6002de05f52ce2f69844432c6494308b06f1c04
App-Developer <rafiullahkhaleel@gmail.com>
2025-04-15 07:41:28 -0700

    commit

--- lib/view/screens/new_record.dart
+++ lib/view/screens/new_record.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -26,7 +27,11 @@ class _NewRecordState extends State<NewRecord> {
         centerTitle: true,
       ),
       body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: width*.07),
+        padding: EdgeInsets.only(
+          left: width * .07,
+          right: width * .07,
+          top: height * .05,
+        ),
         child: Column(
           children: [
             Row(
@@ -34,29 +39,99 @@ class _NewRecordState extends State<NewRecord> {
               children: [
                 Column(
                   children: [
-                    SizedBox(height: height*.03,),
-                    Text('Name',style: GoogleFonts.poppins(
-                      fontSize: 20,
-                      fontWeight: FontWeight.w400,
-                      color: Color(0xFF292D32)
-                    ),),
+                    SizedBox(height: height * .03),
+                    Text(
+                      'Name',
+                      style: GoogleFonts.poppins(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w400,
+                        color: Color(0xFF292D32),
+                      ),
+                    ),
                   ],
                 ),
-                SizedBox(width: width*.15,),
+                SizedBox(width: width * .15),
                 SizedBox(
-                  width: width*.44,
-                    child: TextField(
-                      decoration: InputDecoration(
-                        hintText: 'website or app name',
-                        hintStyle: GoogleFonts.poppins(
-                          fontWeight: FontWeight.w300,
-                          fontSize: 16,
-                          color: Color(0xFF808080)
-                        )
+                  width: width * .44,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: 'website or app name',
+                      hintStyle: GoogleFonts.poppins(
+                        fontWeight: FontWeight.w300,
+                        fontSize: 16,
+                        color: Color(0xFF808080),
                       ),
-                    )),
-
+                    ),
+                  ),
+                ),
+                SizedBox(width: width * .05),
+                Column(
+                  children: [
+                    SizedBox(height: height * .02),
+                    ImageIcon(AssetImage('assets/tick-circle.png')),
+                  ],
+                ),
               ],
+            ),
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Column(
+                  children: [
+                    SizedBox(height: height * .03),
+                    Text(
+                      'User id',
+                      style: GoogleFonts.poppins(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w400,
+                        color: Color(0xFF292D32),
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(width: width * .135),
+                SizedBox(
+                  width: width * .44,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: 'username or email id',
+                      hintStyle: GoogleFonts.poppins(
+                        fontWeight: FontWeight.w300,
+                        fontSize: 16,
+                        color: Color(0xFF808080),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(width: width * .05),
+                Column(
+                  children: [
+                    SizedBox(height: height * .02),
+                    ImageIcon(AssetImage('assets/tick-circle.png')),
+                  ],
+                ),
+              ],
+            ),
+            SizedBox(height: height * .035),
+            Divider(thickness: 2.5,color: Color(0xFFDADADA),),
+            SizedBox(height: height * .015),
+            Text('Password',style: GoogleFonts.poppins(
+              fontSize: 20,
+              fontWeight: FontWeight.w400,
+              color: Color(0xFF292D32)
+            ),),
+            SizedBox(height: height * .015),
+            TextField(
+              decoration: InputDecoration(
+                suffixIcon: ImageIcon(
+                  AssetImage('assets/refresh-2.png')
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(5),
+                  borderSide: BorderSide(color: Color(0xFF9B9B9B))
+                )
+                    
+              ),
             )
           ],
         ),
